56697a5c7eebcff404f5abc14fc8cdf698b0f0f3
Pieter Beulque <pieterbeulque@gmail.com>
2017-07-17 13:57:08 +0200

    Render page template Twig view

--- page.php
+++ page.php
@@ -4,4 +4,8 @@ $context = Timber::get_context();
 
 $context['page'] = $page = new TimberPost();
 
-Timber::render(array('page-' . $page->post_name . '.twig', 'page.twig'), $context);
+Timber::render(array(
+	'page-' . $page->post_name . '.twig',
+	pathinfo(get_page_template(), PATHINFO_FILENAME) . '.twig',
+	'page.twig'
+), $context);

f02e2c8902d584cbdd4b26f5027d30781595a2cb
Pieter Beulque <pieterbeulque@gmail.com>
2017-07-17 13:51:40 +0200

    Fix functions.php

--- functions.php
+++ functions.php
@@ -2,15 +2,7 @@
 
 require_once 'lib/config/i18n/i18n.php';
 
-require_once 'lib/config/admin.php';
-require_once 'lib/config/cleanup.php';
-require_once 'lib/config/media.php';
 require_once 'lib/config/menus.php';
 require_once 'lib/config/seo.php';
 
 require_once 'lib/twig/icon.php';
-require_once 'lib/twig/imgix.php';
-require_once 'lib/twig/sanitize.php';
-require_once 'lib/twig/vimeo.php';
-require_once 'lib/twig/youtube.php';
-
